668eb87be8521bb0cd59711363b590067fba4e93
David Selassie <selassid@gmail.com>
2014-08-23 15:14:05 -0700

    Adds shortest path finding.

--- Assets/LevelController.cs
+++ Assets/LevelController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -24,25 +25,68 @@ public class LevelController : MonoBehaviour {
 
 	public void PerformTurnUpdate () {
 		foreach (CharacterComponent character in this.characters) {
-			IList<IslandComponent> nextIslands = this.AllIslandsAccessibleFromIsland(character.island);
-			if (nextIslands.Count > 0) {
-				IslandComponent nextIsland = nextIslands[0];
-				character.MoveToIsland(nextIsland);
-			}
+			this.UpdateCharacter(character);
 		}
 	}
 
-	void UpdateCharacter (HomingCharacterComponent character) {
-
+	void UpdateCharacter (CharacterComponent character) {
+		IslandComponent home = GameObject.FindGameObjectWithTag("HomeIsland").GetComponent<IslandComponent>();
+		try {
+			IList<IslandComponent> foundPath = this.ShortestBridgePath(character.island, home);
+			if (foundPath.Count > 0) {
+				character.MoveToIsland(foundPath[0]);
+			}
+		} catch (NoPathException) {
+			// Do nothing, no path...
+		}
 	}
 
-	IList<IslandComponent> ShortestBridgePath (IslandComponent from, IslandComponent to) {
-//		if (to == from) {
-//			return new List<IslandComponent>(0);
-//		} else if () {
-//
-//		}
-		return null;
+	List<IslandComponent> ShortestBridgePath (IslandComponent from, IslandComponent to) {
+		return this.ShortestBridgePath(from, to, new List<IslandComponent>(0));
+	}
+
+	private class NoPathException : Exception {
+		public IslandComponent from;
+		public IslandComponent to;
+
+		public NoPathException(IslandComponent from, IslandComponent to) {
+			this.from = from;
+			this.to = to;
+		}
+	}
+
+	private List<IslandComponent> ShortestBridgePath (IslandComponent from, IslandComponent to, List<IslandComponent> pathToFrom) {
+		List<IslandComponent> foundPath = new List<IslandComponent>(pathToFrom);
+		if (to == from) {
+			// Muffin. foundPath is already correct.
+		} else if (this.AllIslandsAccessibleFromIsland(from).Contains(to)) {
+			foundPath.Add(to);
+		} else {
+			List<List<IslandComponent>> paths = new List<List<IslandComponent>>();
+			foreach (IslandComponent possibleNext in this.AllIslandsAccessibleFromIsland(from)) {
+				if (!pathToFrom.Contains(possibleNext)) {
+					try {
+						List<IslandComponent> pathToPossibleNext = new List<IslandComponent>(pathToFrom);
+						pathToPossibleNext.Add(possibleNext);
+						paths.Add(this.ShortestBridgePath(possibleNext, to, pathToPossibleNext));
+					} catch (NoPathException) {
+						// That was a dead end.
+					}
+				}
+			}
+			if (paths.Count > 0) {
+				int minLength = this.islands.Count + 1;
+				foreach (List<IslandComponent> path in paths) {
+					if (path.Count < minLength) {
+						foundPath = path;
+						minLength = path.Count;
+					}
+				}
+			} else {
+				throw new NoPathException(from, to);
+			}
+		}
+		return foundPath;
 	}
 
 	IList<BridgeComponent> AllBridgesTouchingIsland (IslandComponent island) {
