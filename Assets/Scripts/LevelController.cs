dc153d16d83cc899f134a45de149803c66c174d5
David Selassie <selassid@gmail.com>
2014-08-24 16:12:49 -0700

    Adds score window.

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class LevelController : MonoBehaviour
 {
@@ -155,5 +156,22 @@ public class LevelController : MonoBehaviour
                 BuildBridge (clickedIsland, this.mouseDownIsland);
             }
         }
+    }
+
+    public int CurrentScore (WorldState state)
+    {
+        Dictionary<IslandComponent, int> population = new Dictionary<IslandComponent, int> ();
+        foreach (CharacterComponent character in state.characters) {
+            int lastPopulation = 0;
+            population.TryGetValue (character.island, out lastPopulation);
+            population.Add (character.island, lastPopulation + 1);
+        }
+        return population.Values.Max ();
+    }
+
+    public void OnGUI ()
+    {
+        WorldState state = this.FindState ();
+        GUI.Box (new Rect (10, 10, 200, 25), String.Format ("Current Score: {0}", this.CurrentScore (state)));
     }
 }
