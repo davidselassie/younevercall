b1282dc28463f058f833d3b4f4731ae6a9f59c00
David Selassie <selassid@gmail.com>
2014-08-24 15:17:59 -0700

    Adds little kid follow once behavior.

---
+++ Assets/FollowOnceCharacterComponent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowOnceCharacterComponent : CharacterComponent {
+    private CharacterComponent lastTouched = null;
+
+    public override void TurnUpdate (WorldState state) {
+        if (!this.lastTouched) {
+            foreach (CharacterComponent character in state.characters) {
+                if (character.name != this.name && character.island == this.island) {
+                    this.lastTouched = character;
+                }
+            }
+        } else {
+            this.MoveToIsland(this.lastTouched.island);
+            this.lastTouched = null;
+        }
+    }
+}
