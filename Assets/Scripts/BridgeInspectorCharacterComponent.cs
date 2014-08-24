b1838d378033a067ba23d32c12aff7234102e268
David Selassie <selassid@gmail.com>
2014-08-24 16:53:52 -0700

    Renames bridge inspector character controller to component to match everything else.

--- Assets/Scripts/BridgeInspectorCharacterComponent.cs
+++ Assets/Scripts/BridgeInspectorCharacterComponent.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class BridgeInspectorCharacterController : CharacterComponent
+public class BridgeInspectorCharacterComponent : CharacterComponent
 {
     public override void TurnUpdate (WorldState state)
     {
