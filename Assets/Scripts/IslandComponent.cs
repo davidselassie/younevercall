1f2d32bb4f1ad7527d41e26b5c89e7950958df97
Nathan <nathanhinchey@gmail.com>
2014-08-24 18:15:08 -0700

    Not sure what I changed

--- Assets/Scripts/IslandComponent.cs
+++ Assets/Scripts/IslandComponent.cs
@@ -1,6 +1,20 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class IslandComponent : MonoBehaviour
 {
+    public List<GameObject> standingPoints;
 
+    public GameObject FindOpenSpot(){
+
+        for (int i = 0; i < standingPoints.Count; i++) {
+            if (standingPoints[i].GetComponent<StandingPoint>().occupied == false){
+                Debug.Log("found an empty spot." + standingPoints[i]);
+                return standingPoints[i];
+            }
+        }
+        Debug.Log("NO EMPTY SPOTS FOUND");
+        return this.gameObject;
+    }
 }
