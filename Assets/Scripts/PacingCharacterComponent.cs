0f91dfa3d8d6e7ad76a2f07a7c470e36febaaf6d
David Selassie <selassid@gmail.com>
2014-08-24 17:03:17 -0700

    Adds pacing behavior.

---
+++ Assets/Scripts/PacingCharacterComponent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PacingCharacterComponent : CharacterComponent
+{
+    public IslandComponent pingIsland;
+    public IslandComponent pongIsland;
+    private IslandComponent currentTarget;
+
+    void Start ()
+    {
+        this.currentTarget = this.island == this.pingIsland ? this.pongIsland : this.pingIsland;
+    }
+
+    public override void TurnUpdate (WorldState state)
+    {
+        if (this.island == this.currentTarget) {
+            this.currentTarget = this.currentTarget == this.pingIsland ? this.pongIsland : this.pingIsland;
+        }
+        this.MoveTowardsIsland (state, this.currentTarget);
+    }
+}
