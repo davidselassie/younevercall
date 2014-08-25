c523314fb2fcb7817447690228bc4eabc1680955
David Selassie <selassid@gmail.com>
2014-08-24 18:11:39 -0700

    Working waypoints.

--- Assets/Scripts/IslandComponent.cs
+++ Assets/Scripts/IslandComponent.cs
@@ -1,6 +1,18 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class IslandComponent : MonoBehaviour
 {
+    public List<WaypointComponent> waypoints;
 
+    public WaypointComponent UnoccupiedWaypoint ()
+    {
+        foreach (WaypointComponent waypoint in this.waypoints) {
+            if (!waypoint.occupiedBy) {
+                return waypoint;
+            }
+        }
+        return null;
+    }
 }
